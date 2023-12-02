d40fb494d35174923e5cbdf542fe93c2a864a4e1
Thales Granja <thalesgranja.dev@gmail.com>
2023-12-01 21:07:57 -0300

    implementacao do js no projeto

---
+++ storage copy.js
@@ -0,0 +1,16 @@
+
+function setItem(key, value) {
+    return localStorage.setItem(key, value);
+}
+
+function getItem(key) {
+    return localStorage.getItem(key);
+}
+
+function removeItem(key) {
+    return localStorage.removeItem(key);
+}
+
+function clearItems() {
+    return localStorage.clear();
+}
